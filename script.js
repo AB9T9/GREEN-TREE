24aa7e45facec9065773470e5ea232cf401cf6c9
AB SIDDIK <abft102ctg@gmai.com>
2025-09-08 01:56:17 +0600

    categories and add to card ready

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const catContainer = document.getElementById("categories-container");
+
+//! taking id to add to card
+
+let total = 0;
+const totalCount = 0;
+
 //!  card fetching
 
 const cardItems = () => {
@@ -7,13 +14,50 @@ const cardItems = () => {
       cardShow(data.plants);
     });
 };
+
+//! categories fetching
+const getCategories = () => {
+  fetch(`https://openapi.programming-hero.com/api/categories`)
+    .then((res) => res.json())
+    .then((data) => {
+      const categories = data.categories;
+      categories.forEach((names) => {
+        catContainer.innerHTML += `
+        <li id=${names.id} class="text-[#1F2937] p-2 mt-2 hover:bg-[#34eb6e] ">${names.category_name}</li>
+        `;
+      });
+    });
+};
+
+//! add  effect to the element
+catContainer.addEventListener("click", (e) => {
+  if (e.target.localName == "li") {
+    const li = e.target.id;
+    document.querySelectorAll("li").forEach((rem) => {
+      rem.classList.remove("bg-[#34eb6e]");
+    });
+    e.target.classList.add("bg-[#34eb6e]");
+
+    loadTrees(li);
+  }
+});
+
+const loadTrees = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const allPlants = data.plants;
+      cardShow(allPlants);
+    });
+};
+
 //! card show in UI
 const cardShow = (getData) => {
   const fruitsCardParent = document.getElementById("middle-card-parent");
   fruitsCardParent.innerHTML = "";
   getData.forEach((fruit) => {
     fruitsCardParent.innerHTML += `
-     <div class="card bg-base-100 shadow-sm " id="${fruit.id}">
+     <div class="card bg-base-100 shadow-sm addBtn" id="${fruit.id}">
       <figure>
         <img
           src="${fruit.image}"
@@ -32,10 +76,10 @@ const cardShow = (getData) => {
           >
             ${fruit.category}
           </div>
-          <h1 class="text-xl font-bold">৳<span>${fruit.price}</span></h1>
+          <h1 class="text-xl font-bold">${fruit.price}</h1>
         </div>
       </div>
-      <button class="w-[90%] mx-auto my-2  bg-[#15803D] rounded-full p-3 text-white">
+      <button onclick="showItem(${fruit.price},'${fruit.name}','${fruit.name}')" class="w-[90%] mx-auto my-2  bg-[#15803D] rounded-full p-3 text-white">
         Add to Cart
       </button>
     </div>
@@ -43,4 +87,36 @@ const cardShow = (getData) => {
   });
 };
 
+//! making add to card
+const cartContainer = document.getElementById("card-parent");
+
+const showItem = (price, product, productName) => {
+  const div = document.createElement("div");
+  div.innerHTML = `  <div
+              class="flex justify-between items-center bg-[#F0FDF4] p-2 rounded-md mt-2"
+            >
+              <div class="flex flex-col ga-2">
+              <h1 class="font-bold">${product}</h1>
+                <h1>
+                  <span>${price}</span><span class="cursor-pointer"> x </span
+                  ><span>1</span>
+                </h1>
+              </div>
+              <div><span class="delete">❌</span></div>
+            </div> 
+            `;
+
+  total += price;
+  document.getElementById("total").innerText = total;
+  cartContainer.append(div);
+  alert(productName + " is added");
+  div.querySelector(".delete").addEventListener("click", () => {
+    div.remove();
+    total -= price;
+    alert(productName + " is removed");
+    document.getElementById("total").innerText = total;
+  });
+};
+
 cardItems();
+getCategories();
